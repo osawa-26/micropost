25568def76c874601559923fc398b644fb524202
jiro <ec2-user@ip-172-31-42-225.ec2.internal>
2020-03-14 08:54:43 +0000

    0314

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    micropost = Micropost.find(params[:id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました。'
     redirect_back(fallback_location: root_url)
