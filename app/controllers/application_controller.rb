844704b65dd8c6ebf7581f119314f7c2efd6973f
jiro <ec2-user@ip-172-31-42-225.ec2.internal>
2020-03-14 07:16:47 +0000

    0314

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,19 @@
 class ApplicationController < ActionController::Base
 
-    include SessionsHelper
+  include SessionsHelper
 
-    private
+  private
 
-    def require_user_logged_in
-        unless logged_in?
-            redirect_to login_url
-        end
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
     end
-    
-    def counts(user)
-        @count_microposts = user.microposts.count
-    end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+    @count_favoritings = user.favoritings.count
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_014738) do
+ActiveRecord::Schema.define(version: 2020_03_08_101134) do
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "micropost_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "favorite_id_id"
+    t.index ["favorite_id_id"], name: "index_favorites_on_favorite_id_id"
+    t.index ["micropost_id"], name: "index_favorites_on_micropost_id"
+    t.index ["user_id", "micropost_id"], name: "index_favorites_on_user_id_and_micropost_id", unique: true
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,6 +30,16 @@ ActiveRecord::Schema.define(version: 2020_02_24_014738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -28,5 +50,9 @@ ActiveRecord::Schema.define(version: 2020_02_24_014738) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "microposts"
+  add_foreign_key "favorites", "users"
   add_foreign_key "microposts", "users"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
